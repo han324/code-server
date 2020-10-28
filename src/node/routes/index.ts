d8fe245ffa208e763763d5f518b5687b70d72226
Teffen Ellis <teffen@nirri.us>
2020-10-28 17:13:52 -0400

    Refactor template string with handlebars.
    - Refactor static routing, template vars.
    - Fixed issue where JSON entities may be unescaped
    - Updated web manifest schema, route handler.
    - Clean up issues surrounding static paths, types.
    - Removed static path helper.
    - Fixed issue where locals may change during request handling.
    - Added missing types.
    - Fixed issue where body theme color is stale.

--- src/node/routes/index.ts
+++ src/node/routes/index.ts
@@ -11,12 +11,13 @@ import { plural } from "../../common/util"
 import { AuthType, DefaultedArgs } from "../cli"
 import { rootPath } from "../constants"
 import { Heart } from "../heart"
-import { replaceTemplates } from "../http"
+import { commonTemplateVars } from "../http"
 import { loadPlugins } from "../plugin"
 import * as domainProxy from "../proxy"
 import { getMediaMime, paths } from "../util"
 import * as health from "./health"
 import * as login from "./login"
+import * as manifest from "./manifest"
 import * as proxy from "./proxy"
 // static is a reserved keyword.
 import * as _static from "./static"
@@ -85,12 +86,14 @@ export const register = async (app: Express, server: http.Server, args: Defaulte
 
   app.use("/", domainProxy.router)
   app.use("/", vscode.router)
+  app.use("/", manifest.router)
+  app.use("/", _static.router)
+
   app.use("/healthz", health.router)
   if (args.auth === AuthType.Password) {
     app.use("/login", login.router)
   }
   app.use("/proxy", proxy.router)
-  app.use("/static", _static.router)
   app.use("/update", update.router)
   app.use("/vscode", vscode.router)
 
@@ -101,20 +104,20 @@ export const register = async (app: Express, server: http.Server, args: Defaulte
   })
 
   const errorHandler: ErrorRequestHandler = async (err, req, res, next) => {
-    const resourcePath = path.resolve(rootPath, "src/browser/pages/error.html")
-    res.set("Content-Type", getMediaMime(resourcePath))
+    if (err.code === "ENOENT" || err.code === "EISDIR") {
+      err.status = HttpCode.NotFound
+    }
+
+    const status = err.status ?? err.statusCode ?? 500
+
     try {
-      const content = await fs.readFile(resourcePath, "utf8")
-      if (err.code === "ENOENT" || err.code === "EISDIR") {
-        err.status = HttpCode.NotFound
-      }
-      const status = err.status ?? err.statusCode ?? 500
-      res.status(status).send(
-        replaceTemplates(req, content)
-          .replace(/{{ERROR_TITLE}}/g, status)
-          .replace(/{{ERROR_HEADER}}/g, status)
-          .replace(/{{ERROR_BODY}}/g, err.message),
-      )
+      res.status(status).render("error/index", {
+        ...commonTemplateVars(req),
+        HOME_PATH: (typeof req.query.to === "string" && req.query.to) || "/",
+        ERROR_TITLE: status,
+        ERROR_HEADER: status,
+        ERROR_BODY: err.message,
+      })
     } catch (error) {
       next(error)
     }
